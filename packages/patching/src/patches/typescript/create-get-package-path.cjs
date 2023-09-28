cf2b7b681fec439f219d2e02538dc393a71f48db
Leon Si <leon@leondreamed.com>
2023-09-27 23:10:37 -0400

    chore: tilde imports

--- packages/patching/src/patches/typescript/create-get-package-path.cjs
+++ packages/patching/src/patches/typescript/create-get-package-path.cjs
@@ -1,16 +1,23 @@
 // @ts-check
 
-const { globSync } = require('glob');
-const { getMonorepoDirpath } = require('get-monorepo-root');
+const { globSync } = require("glob");
+const { getMonorepoDirpath } = require("get-monorepo-root");
+const { createTildeImportExpander } = require("tilde-imports");
 
 module.exports = function createGetPackagePath() {
-	const monorepoDirpath = getMonorepoDirpath(__dirname)
-	if (monorepoDirpath === undefined) {
-		throw new Error('Could not find monorepo directory');
-	}
+  const monorepoDirpath = getMonorepoDirpath(__dirname);
+  if (monorepoDirpath === undefined) {
+    throw new Error("Could not find monorepo directory");
+  }
 
-	return function (directoryPath) {
+  const expandTildeImport = createTildeImportExpander({
+    monorepoDirpath,
+  });
 
-	};
+  return function (directory: string) {
+    expandTildeImport({
+      importSpecifier: "~",
+      importerFilepath: directory,
+    });
+  };
 };
-
