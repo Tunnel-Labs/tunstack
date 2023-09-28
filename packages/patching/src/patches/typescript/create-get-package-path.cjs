103213623ac19ba0ac48689c806a65a2cbc4ae50
Leon Si <leon@leondreamed.com>
2023-09-27 23:08:10 -0400

    chore: get package path

---
+++ packages/patching/src/patches/typescript/create-get-package-path.cjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+const { globSync } = require('glob');
+const { getMonorepoDirpath } = require('get-monorepo-root');
+
+module.exports = function createGetPackagePath() {
+	const monorepoDirpath = getMonorepoDirpath(__dirname)
+	if (monorepoDirpath === undefined) {
+		throw new Error('Could not find monorepo directory');
+	}
+
+	return function (directoryPath) {
+
+	};
+};
+
